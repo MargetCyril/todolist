89f6fd3371e17f9764d86e87d0f0e1b330a28f40
MargetCyril <cyrilmarget@gmail.com>
2024-05-29 16:52:35 +0200

    29/05 17h

--- script.js
+++ script.js
@@ -1,30 +1,14 @@
 function get_json() {
-   let client = document.getElementById("client").value;
-   client = "myclient: "+ client
-   const myJSON = JSON.stringify(client)
+
+   let client ="'client: '"+ document.getElementById("client").value;
+   let commande ="'commande: '"+ document.getElementById("commande").value
+   let recu ="'recu: '"+ document.getElementById("recu").value
+   let limite ="'limite: '"+ document.getElementById("limite").value
+
+   const myJSON = JSON.stringify(client +","+ commande +","+ recu +","+ limite)
    console.log(myJSON)
 }
 
-/*   Récupérer les données du formulaire et générer des paires clef/valeur dans un 'objet':
+/* Récupérer les données du formulaire et générer des paires clef/valeur dans un 'objet':
    { "clef": "valeur" ... }
-    Sauvegarder l'objet dans un cookie
-    Ajouter un bouton sauvegarder pour demander à l'utilisateur où stocker le fichier. */
-
-    async function sauvegarder() {
-      try {
-        // create a new handle
-        const newHandle = await window.showSaveFilePicker();
-    
-        // create a FileSystemWritableFileStream to write to
-        const writableStream = await newHandle.createWritable();
-    
-        // write our file
-        await writableStream.write("This is my file content");
-    
-        // close the file and write the contents to disk.
-        await writableStream.close();
-      } catch (err) {
-        console.error(err.name, err.message);
-      }
-    }
-    
+    Sauvegarder l'objet dans un cookie/localstorage */
